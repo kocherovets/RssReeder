5a4acbceebb614f863b5e7dc0c651d0beaa43cfe
kocherovets <kocherovets@gmail.com>
2020-11-06 20:42:46 +0300

    refactoring

---
+++ RssSwiftUI/RssSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RssSwiftUIApp.swift
+//  RssSwiftUI
+//
+//  Created by Dmitry Kocherovets on 06.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct RssSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
